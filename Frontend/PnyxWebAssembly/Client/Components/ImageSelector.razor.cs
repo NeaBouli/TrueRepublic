684fdc81a711a547ec2407c8d96745e8e7fde578
xxlfan72 <andreasschurz@andreasschurz.de>
2022-04-02 01:50:43 +0200

    Added demo selection logic

--- Frontend/PnyxWebAssembly/Client/Components/ImageSelector.razor.cs
+++ Frontend/PnyxWebAssembly/Client/Components/ImageSelector.razor.cs
@@ -1,19 +1,49 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Options;
 using MudBlazor;
 
 namespace PnyxWebAssembly.Client.Components
 {
+    public class ImageItem
+    {
+        public int Id { get; set; }
+    }
+
     public partial class ImageSelector
     {
         [CascadingParameter] 
-        MudDialogInstance MudDialog { get; set; }
+        private MudDialogInstance MudDialog { get; set; }
+
+        [Inject]
+        private ISnackbar Snackbar { get; set; }
 
         [Parameter] 
         public string Hashtags { get; set; }
 
         private int SelectedPaper { get; set; }
 
-        private void Submit() => MudDialog.Close(DialogResult.Ok(true));
+        public List<ImageItem> ImageItems { get; } = new List<ImageItem>();
+
+        public ImageSelector()
+        {
+            for (int i = 0; i < 96; i++)
+            {
+                ImageItems.Add(new ImageItem
+                {
+                    Id = i
+                });
+            }
+        }
+
+        private void Submit()
+        {
+            MudDialog.Close(DialogResult.Ok(true));
+
+            Snackbar.Clear();
+            Snackbar.Configuration.PositionClass = Defaults.Classes.Position.TopCenter;
+            Snackbar.Add($"Selected Item: {SelectedPaper}");
+        }
 
         private void Cancel() => MudDialog.Cancel();
     }
