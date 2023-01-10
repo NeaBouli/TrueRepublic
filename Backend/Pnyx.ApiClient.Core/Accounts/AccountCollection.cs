9c068e737232508c290aa898cca5e019d2de2f21
ingjuedt <ingbert.juedt@non.agilent.com>
2023-01-10 15:38:44 +0100

    Account helper class

---
+++ Backend/Pnyx.ApiClient.Core/Accounts/AccountCollection.cs
@@ -0,0 +1,38 @@
+﻿using Pnyx.ApiClient.Core.Accounts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pnyx.SmartContracts.Solidity.Contracts.Accounts
+{
+    public class AccountCollection<AccountType> where AccountType : PnyxAccount
+    {
+        private static Dictionary<string, AccountType> _accounts = new Dictionary<string, AccountType>();
+
+        public bool AddAccount(AccountType account)
+        {
+            bool containsKey = _accounts.ContainsKey(account.ID);
+            if (!containsKey)
+            {
+                _accounts[account.ID] = account;
+            }
+            return containsKey;
+        }
+
+        public AccountType GetAccount(string accountID)
+        {
+            if (_accounts.ContainsKey(accountID))
+            {
+                return _accounts[accountID];
+            }
+            return null;
+        }
+
+        public bool RemoveAccount(AccountType account)
+        {
+            return _accounts.Remove(account.ID);
+        }
+    }
+}
---
+++ Backend/Pnyx.SmartContracts.Solidity/Accounts/ETHPnyxAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using Pnyx.ApiClient.Core.Accounts;
+using Nethereum.Web3.Accounts;
+using Nethereum.Signer;
+
+namespace Pnyx.SmartContracts.Solidity.Contracts.Accounts
+{
+    public class ETHPnyxAccount : PnyxAccount
+    {
+        public static ETHPnyxAccount Create(string privateKey, BigInteger? chainId = null)
+        {
+            return new ETHPnyxAccount(privateKey, chainId);
+        }
+
+        public ETHPnyxAccount(string privateKey, BigInteger? chainId = null)
+            : base()
+        {
+            ETHAccount = new Account(privateKey, chainId);
+        }
+
+        public Account ETHAccount { get; }
+
+        public PnyxAccountRoles AccountRole { get; }
+    }
+}
